28aa6a7106552dad3f636354661d127289e0794b
GongYuhua <gongyuhua@gmail.com>
2025-02-11 12:22:33 +0800

    chore: lower minimum supported system version to iOS 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XYZAuthKit",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "XYZAuthKit",
